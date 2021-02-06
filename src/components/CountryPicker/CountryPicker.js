8c0e008de26a1b4fa4bb052e6e5909d8217ab9ed
Samir <samirkr2527@gmail.com>
2021-02-06 21:24:07 +0530

    fixed break issue in dropdown

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,39 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { NativeSelect, FormControl } from "@material-ui/core";
-import styles from './CountryPicker.module.css'
-import {fetchCountries} from '../../api/index'
+import styles from "./CountryPicker.module.css";
+import { fetchCountries } from "../../api/index";
 
-function CountryPicker({handleCountryChange})
-{
+function CountryPicker({ handleCountryChange }) {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
 
-   const [fetchedCountries,setFetchedCountries]=useState([]);
-
-   /*
+  /*
     useEffect(): hook to create sideEffects in react
     after every single render and update useEffect() will be called
     
     dependancy list:we supply extra argument for conditinal execution of useEffect()
    */
 
-   useEffect(() => {
-       const fetchAPI = async () =>{
-           setFetchedCountries(await fetchCountries())
-       }
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
 
-       fetchAPI();
-   },[fetchedCountries]);
+    fetchAPI();
+  }, [fetchedCountries]);
 
-    return (
-        <div style={{display: 'flex', justifyContent: 'center'}}> 
-
-           <FormControl className={styles.formControl}>
-               <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                   <option value="global">Global</option>
-                   {fetchedCountries.map((country,i) => <option key={i} value={country}>{country}</option>)}
-               </NativeSelect>
-           </FormControl>
-        </div>
-    )
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <FormControl className={styles.formControl}>
+        <NativeSelect
+          defaultValue=""
+          onChange={(e) => handleCountryChange(e.target.value)}
+        >
+          <option value="">Global</option>
+          {fetchedCountries.map((country, i) => (
+            <option key={i} value={country}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
 }
 
-export default CountryPicker
+export default CountryPicker;
